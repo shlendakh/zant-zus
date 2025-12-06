e1653ebddb03b53e22c03b3b5f506204d36089e7
Mobile App Solutions <oszubski.dawid@gmail.com>
2025-12-06 16:09:01 +0100

    init commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+const { heroui } = require("@heroui/react");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@heroui/react/dist/**/*.{js,ts,jsx,tsx}", 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    heroui(), 
+  ],
+};
