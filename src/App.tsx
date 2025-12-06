0596beb41a7130f4a3a83262bfb7ed4c1db4d28f
Mobile App Solutions <oszubski.dawid@gmail.com>
2025-12-06 17:36:09 +0100

    changed default theme mode to System

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { ThemeProvider, ThemeMode } from "@/components/theme/theme-provider"
 
 function App() {
   return (
-    <ThemeProvider defaultTheme={ThemeMode.Dark} storageKey="vite-ui-theme">
+    <ThemeProvider defaultTheme={ThemeMode.System} storageKey="vite-ui-theme">
     <div className="min-h-screen">
       <AppBar />
       <main className="container mx-auto px-3 sm:px-4 md:px-6 py-4 sm:py-6 md:py-8">
